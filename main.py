625f9568eb3377e6979b2ee9909cedbaae600268
Kamran <130050163+Chamranz@users.noreply.github.com>
2025-05-19 08:31:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,9 @@ def predict(request: BatchPredictionRequest):
     try:
         features_list = [item.dict() for item in request.data]
         predictions = predicted_prices(model, features_list)
-        return {"predictions": predictions}
+        predictions = predicted_prices(model, features_list)
+        formatted_predictions = [f"{p:.2f} $" for p in predictions]
+        return {"predictions": formatted_predictions}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Ошибка предсказания: {str(e)}")
 
